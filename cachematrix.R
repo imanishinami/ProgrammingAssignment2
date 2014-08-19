03477ed7f1bd59a7db179bbdb7593e161407c7dc
Imanishi Nami <imanishi@tp.edu.sg>
2014-08-19 11:12:34 +0800

    Update cachematrix.R
    just follow makeVector function to change to makeCacheMatrix ....

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    inv <- NULL
+    set <- function(matrix) {
+        x <<- matrix
+        inv <<- NULL
+    }
+    get <- function() {
+        x 
+    }
+    setInverse <- function(inverse) {
+        inv <<- inverse
+    }
+    getInverse <- function() {
+        inv
+    }
+    list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse
+    }        
 }
 
 
